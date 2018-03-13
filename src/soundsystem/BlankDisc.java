b281d6c81f0bf9228b5087cfd0c3efd4b7e68da0
xingpk <xpeikang@gmail.com>
2018-03-13 16:43:55 +0800

    Initial commit

---
+++ src/soundsystem/BlankDisc.java
@@ -0,0 +1,15 @@
+package soundsystem;
+
+public class BlankDisc implements CompactDisc {
+    private String title;
+    private String artist;
+
+    public BlankDisc(String title){
+        this.title = title;
+//        this.artist = artist;
+    }
+
+    public void play(){
+
+    }
+}
